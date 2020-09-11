16fcd91a4144760278126e77e94c177a369b4ea0
syanhai_yakisoba=\chan! <koumaza--@outlook.com>
2020-09-11 15:39:00 +0000

    First Commit

---
+++ CreateCat.cmd
@@ -0,0 +1,5 @@
+@echo off
+echo WDK Required
+REM rm Packages\Drivers\Display\WT6A_INF\*.cat
+set /p i2cpath="Enter Inf2cat.exe path: "
+"%i2cpath%"\Inf2cat.exe /Driver:Packages\Drivers\Display\WT6A_INF\ /OS:10_X64
