d905c31b38dc0a198b8b73e7e7231fb858d04c52
shanghai yakisoba chan! <koumaza--@outlook.com>
2020-09-12 17:44:05 +0900

    Update CreateCat.cmd

--- CreateCat.cmd
+++ CreateCat.cmd
@@ -3,3 +3,4 @@ echo WDK Required
 REM rm Packages\Drivers\Display\WT6A_INF\*.cat
 set /p i2cpath="Enter Inf2cat.exe path: "
 "%i2cpath%"\Inf2cat.exe /Driver:Packages\Drivers\Display\WT6A_INF\ /OS:10_X64
+pause
